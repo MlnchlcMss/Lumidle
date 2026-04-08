a11f21b6c93c57235fb0ba15c8243a1f56993ee3
MlnchlcMss <91789596+MlnchlcMss@users.noreply.github.com>
2026-04-08 11:14:55 +0800

    erm

--- lumidle/vite.config.js
+++ lumidle/vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Lumidle",
   server: {
     proxy: {
       '/api': 'http://localhost:5000' 
     }
   }
-})    
+})  
